2d2e5aecf142cde2526da22be992815fc9b88544
Ricardo de Carvalho <skapitao@gmail.com>
2024-05-08 22:56:03 -0300

    feat: Config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'inline',
+      manifest: false
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',

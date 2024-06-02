a1d8c10aec1447e535321aeee4578f966e9dc7f1
Ricardo de Carvalho <skapitao@gmail.com>
2024-06-01 21:34:56 -0300

    feat: Expire cookie

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { CookiesProvider } from 'react-cookie';
 
+const currentDate = new Date();
+const expirationDate = new Date();
+
+expirationDate.setFullYear(currentDate.getFullYear() + 10);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CookiesProvider defaultSetOptions={{ path: '/' }}>
+    <CookiesProvider
+      defaultSetOptions={{
+        path: '/',
+        secure: true,
+        expires: expirationDate,
+      }}
+    >
       <App />
     </CookiesProvider>
   </React.StrictMode>
